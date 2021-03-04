b92877291b59054dcc8f468a366fda443203dc23
Ilja Bezrukov <ibezrukov@tuebingen.mpg.de>
2021-03-04 10:35:15 +0100

    Fixed killing mysql server for longer user names

--- kill_running_mysql.bash
+++ kill_running_mysql.bash
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 echo "Soft killing all running user udocker and mysql processes ..."
-parallel 'kill {}' ::: $(ps aux |grep -E 'mysql|udocker' |grep $USER |awk {'print $2'})
+parallel 'kill {}' ::: $(ps -fu $USER |grep -E 'mysql|udocker' | awk {'print $2'})
 
 echo "Waiting 15s"
 sleep 15s
 
 echo "Hard killing all running user udocker and mysql processes ..."
-parallel 'kill -9 {}' ::: $(ps aux |grep -E 'mysql|udocker' |grep $USER |awk {'print $2'})
+parallel 'kill -9 {}' ::: $(ps -fu $USER |grep -E 'mysql|udocker' |awk {'print $2'})
 echo "Done"
